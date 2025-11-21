4a8762c8344ed6985dd7b3c91f601173b4dfb739
Polina <asugjjgjcvgffj@gmail.com>
2025-11-21 11:09:26 +0700

    hello git

---
+++ programming/sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define N 5
+
+void bubble_sort(int size, int arr[size]) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) { 
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
+
+void print_array(int size, int array[size]) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+
+    int array[N] = {6, -5, 3, 77, 4};
+    print_array(N, array);
+
+    bubble_sort(N, array);
+
+    print_array(N, array);
+
+    return 0;
+}
